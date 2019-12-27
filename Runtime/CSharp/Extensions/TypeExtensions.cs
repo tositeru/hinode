74d5406a8a581159d05b6b412ed67380f4b7146e
tositeru <tositeru104@protonmail.com>
2019-12-27 15:20:39 +0900

    System.Typeの拡張メソッド追加

---
+++ Runtime/CSharp/Extensions/TypeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace Hinode
+{
+    public static class TypeExtensions
+    {
+        public static FieldInfo GetFieldInHierarchy(this System.Type t, string name, BindingFlags flags = BindingFlags.Default)
+        {
+            while(t != null)
+            {
+                var info = t.GetField(name, flags);
+                if (info != null)
+                    return info;
+                t = t.BaseType;
+            }
+            return null;
+        }
+    }
+}
---
+++ Tests/Runtime/CSharp/Extensions/TestTypeExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Hinode.Tests.CSharp
+{
+    public class TestTypeExtensions
+    {
+        class GetFieldInHierarchyBaseClass : ScriptableObject
+        {
+#pragma warning disable CS0649, CS0414
+            int value = 1;
+#pragma warning restore CS0649, CS0414
+        }
+        class GetFieldInHierarchySubClass : GetFieldInHierarchyBaseClass
+        {
+#pragma warning disable CS0649, CS0414
+            public int apple = 10;
+#pragma warning restore CS0649, CS0414
+        }
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void GetFieldInHierarchyPasses()
+        {
+            Assert.AreEqual(typeof(GetFieldInHierarchyBaseClass).GetField("value", BindingFlags.NonPublic),
+                typeof(GetFieldInHierarchySubClass).GetFieldInHierarchy("value", BindingFlags.NonPublic));
+            // Use the Assert class to test conditions
+        }
+    }
+}
