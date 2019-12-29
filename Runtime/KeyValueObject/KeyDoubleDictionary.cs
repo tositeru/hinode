5f8aa7462038a847d12407e8b057497564e89ab1
tositeru <tositeru104@protonmail.com>
2019-12-29 20:45:06 +0900

    KeyValueDictionary系の実装

---
+++ Runtime/KeyValueObject/KeyDoubleDictionary.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+namespace Hinode
+{
+    /// <summary>
+    /// <seealso cref="IKeyValueDictionary{TKeyValue, T}"/>
+    /// </summary>
+    [CreateAssetMenu(menuName = "Hinode/KeyValue/Double KeyValueDictionary")]
+    public class KeyDoubleDictionary : IKeyValueDictionary<KeyDoubleObject, double>
+    {
+        protected override KeyDoubleObject CreateObj(string key, double value)
+            => new KeyDoubleObject(key, value);
+
+        public KeyDoubleObject this[string key]
+        {
+            get => Get(key) as KeyDoubleObject;
+        }
+        public new KeyDoubleObject Get(string key) => this[key];
+    }
+}
---
+++ Runtime/KeyValueObject/KeyEnumDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Hinode
+{
+    /// <summary>
+    /// 定義されていたEnum値が削除されたなどの時に対応するため内部データではint型(Enumの名前への添字)を持つように指定います。
+    /// <seealso cref="IKeyValueDictionary{TKeyValue, T}"/>
+    /// <seealso cref="IKeyValueDictionaryWithTypeName{TKeyValue, T}"/>
+    /// </summary>
+    [CreateAssetMenu(menuName = "Hinode/KeyValue/Enum KeyValueDictionary")]
+    public class KeyEnumDictionary : IKeyValueDictionaryWithTypeName<KeyEnumObject, int>
+    {
+        public static KeyEnumDictionary Create<TEnum>()
+            where TEnum : System.Enum
+        {
+            var inst = CreateInstance<KeyEnumDictionary>();
+            Assert.IsTrue(inst.IsValidType(typeof(TEnum)));
+            inst.CurrentType = typeof(TEnum);
+            return inst;
+        }
+
+        public KeyEnumObject this[string key]
+        {
+            get => Get(key) as KeyEnumObject;
+        }
+        public new KeyEnumObject Get(string key) => this[key];
+
+        #region IKeyValueDictionary
+        protected override KeyEnumObject CreateObj(string key, int value)
+            => new KeyEnumObject(key, (System.Enum)(object)value, CurrentType);
+
+        public override void Refresh()
+        {
+            CurrentType = FindType();
+            base.Refresh();
+        }
+        #endregion
+
+        #region IKeyValueDictionaryWithTypeName
+        protected override bool IsValidType(System.Type type) => type?.IsSubclassOf(typeof(System.Enum)) ?? false;
+        #endregion
+    }
+}
