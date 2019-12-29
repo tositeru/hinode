1768bbfa0d8dabc0fb4971ba31e893d2e2446342
tositeru <tositeru104@protonmail.com>
2019-12-29 20:45:59 +0900

    エディター拡張の実装用のクラスを追加
    - SerializedTarget
    - GUILayoutPosition

---
+++ Editor/CustomGUIContent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hinode.Editors
+{
+    /// <summary>
+    /// 追加のパラメータを渡せるようにしたGUIContent
+    /// </summary>
+    public class CustomGUIContent : GUIContent
+    {
+        public object Parameter { get; set; }
+
+        public CustomGUIContent() { }
+        public CustomGUIContent(GUIContent src) : base(src) { }
+        public CustomGUIContent(string text) : base(text) { }
+        public CustomGUIContent(Texture image) : base(image) { }
+        public CustomGUIContent(string text, string tooltip) : base(text, tooltip) { }
+        public CustomGUIContent(string text, Texture image) : base(text, image) { }
+        public CustomGUIContent(Texture image, string tooltip) : base(image, tooltip) { }
+        public CustomGUIContent(string text, Texture image, string tooltip) : base(text, image, tooltip) { }
+    }
+}
---
+++ Runtime/GUILayoutPosition.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Hinode
+{
+    public class GUILayoutPosition
+    {
+        Rect _pos;
+        public Rect Pos { get => _pos; private set => _pos = value; }
+        public float RowHeight { get; private set; }
+        public GUILayoutPosition(Rect position, float rowHeight=-1)
+        {
+            Pos = position;
+            RowHeight = rowHeight <= 0 ? Pos.height : rowHeight;
+        }
+
+        public Rect GetSplitPos(float divideCount, int index, int width=1)
+        {
+            var p = Pos;
+            p.width /= divideCount;
+            p.x += p.width * index;
+            p.width *= width;
+            return p;
+        }
+
+        public void IncrementRow()
+        {
+            GUILayout.Space(RowHeight);
+            _pos.y += RowHeight;
+        }
+
+        public GUILayoutPosition Indent(bool isAuto, float percent=0.1f)
+        {
+            if(isAuto)
+            {
+#if UNITY_EDITOR
+                return new GUILayoutPosition(EditorGUI.IndentedRect(_pos), RowHeight);
+#else
+                percent = 0.1f;
+#endif
+            }
+
+            var offset = _pos.width * percent;
+            var pos = _pos;
+            pos.x += offset;
+            pos.width -= offset;
+            return new GUILayoutPosition(pos, RowHeight);
+        }
+    }
+}
