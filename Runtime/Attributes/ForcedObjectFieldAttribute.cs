ec6adcb79c58ff0150e0c0fb0690cbb781fb0755
tositeru <tositeru104@protonmail.com>
2020-01-01 14:54:11 +0900

    ForceObjectFieldAttributeを実装

---
+++ Runtime/Attributes/ForcedObjectFieldAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hinode
+{
+    /// <summary>
+    /// 強制的にObjectFieldにしたい時に指定するPropertyAttribute
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+    public sealed class ForcedObjectFieldAttribute : PropertyAttribute
+    {
+        readonly bool _allowSceneObjects;
+        public bool AllowSceneObject { get => _allowSceneObjects; }
+
+        public ForcedObjectFieldAttribute(bool allowSceneObjects=true)
+        {
+            _allowSceneObjects = allowSceneObjects;
+        }
+
+        public bool DoValid(object target)
+        {
+            return target is Object;
+        }
+    }
+}
