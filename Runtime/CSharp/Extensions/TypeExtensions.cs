1a24a2499d604cfb5074fc71654697a7617b9a66
tositeru <tositeru104@protonmail.com>
2020-01-08 17:47:17 +0900

    TestTypeExtensionsのメソッド追加 IsArrayOrList, GetArrayElementType, GetClassHierarchyEnumerable

--- Runtime/CSharp/Extensions/TypeExtensions.cs
+++ Runtime/CSharp/Extensions/TypeExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace Hinode
 {
@@ -18,5 +20,76 @@ namespace Hinode
             }
             return null;
         }
+
+        public static bool IsArrayOrList(this System.Type t)
+        {
+            if(t.IsArray) return true;
+            //IList, IList<>から派生しているか？
+            return t.GetInterfaces().Any(_i => _i.Equals(typeof(IList)) || _i.Equals(typeof(IList<>)));
+        }
+
+        public static System.Type GetArrayElementType(this System.Type t)
+        {
+            Assert.IsTrue(t.IsArrayOrList());
+            if (t.IsArray)
+            {
+                return t.GetElementType();
+            }
+
+            var elementType = t.GetInterfaces()
+                .Where(_i => _i.IsGenericType)
+                .Where(_i => _i.GetGenericTypeDefinition().Equals(typeof(IList<>)))
+                .Select(_i => _i.GetGenericArguments()[0])
+                .FirstOrDefault();
+            return elementType ?? typeof(object);
+        }
+
+        public static IEnumerable<System.Type> GetClassHierarchyEnumerable(this System.Type t)
+        {
+            return new ClassHierarchyEnumerable(t);
+        }
+
+        class ClassHierarchyEnumerable : IEnumerable<System.Type>, IEnumerable
+        {
+            System.Type _target;
+            public ClassHierarchyEnumerable(System.Type target)
+            {
+                _target = target;
+            }
+
+            public IEnumerator<System.Type> GetEnumerator()
+            {
+                return new Enumerator(_target);
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+
+            class Enumerator : IEnumerator<System.Type>, IEnumerator, System.IDisposable
+            {
+                System.Type _target;
+                IEnumerator<System.Type> _enumerator;
+                public Enumerator(System.Type target)
+                {
+                    _target = target;
+                    Reset();
+                }
+                public System.Type Current => _enumerator.Current;
+                object IEnumerator.Current => Current;
+                public void Dispose() { }
+                public bool MoveNext() => _enumerator.MoveNext();
+                public void Reset() => _enumerator = GetEnumerator();
+
+                IEnumerator<System.Type> GetEnumerator()
+                {
+                    var t = _target;
+                    while(t != null)
+                    {
+                        yield return t;
+                        t = t.BaseType;
+                    }
+                }
+            }
+        }
+
     }
 }
--- Tests/Runtime/CSharp/Extensions/TestTypeExtensions.cs
+++ Tests/Runtime/CSharp/Extensions/TestTypeExtensions.cs
@@ -30,5 +30,37 @@ namespace Hinode.Tests.CSharp
                 typeof(GetFieldInHierarchySubClass).GetFieldInHierarchy("value", BindingFlags.NonPublic));
             // Use the Assert class to test conditions
         }
+
+        class InheritedList : List<int> {}
+
+        [Test]
+        public void IsArrayOrListPasses()
+        {
+            Assert.IsTrue(typeof(int[]).IsArrayOrList());
+            Assert.IsTrue(typeof(System.Array).IsArrayOrList());
+            Assert.IsTrue(typeof(ArrayList).IsArrayOrList());
+            Assert.IsTrue(typeof(List<>).IsArrayOrList());
+            Assert.IsTrue(typeof(List<int>).IsArrayOrList());
+            Assert.IsTrue(typeof(InheritedList).IsArrayOrList());
+
+            Assert.IsFalse(typeof(int).IsArrayOrList());
+            Assert.IsFalse(typeof(System.Enum).IsArrayOrList());
+            Assert.IsFalse(typeof(Dictionary<string, int>).IsArrayOrList());
+        }
+
+        [Test]
+        public void GetArrayElementTypePasses()
+        {
+            Assert.AreEqual(typeof(int), typeof(int[]).GetArrayElementType());
+            Assert.AreEqual(typeof(object), typeof(System.Array).GetArrayElementType());
+            Assert.AreEqual(typeof(object), typeof(ArrayList).GetArrayElementType());
+            Assert.AreEqual(typeof(List<>).GetGenericArguments()[0], typeof(List<>).GetArrayElementType());
+            Assert.AreEqual(typeof(string), typeof(List<string>).GetArrayElementType());
+            Assert.AreEqual(typeof(int), typeof(InheritedList).GetArrayElementType());
+
+            Assert.Throws<UnityEngine.Assertions.AssertionException>(() => typeof(int).GetArrayElementType());
+            Assert.Throws<UnityEngine.Assertions.AssertionException>(() => typeof(System.Enum).GetArrayElementType());
+            Assert.Throws<UnityEngine.Assertions.AssertionException>(() => typeof(Dictionary<string, int>).GetArrayElementType());
+        }
     }
 }
