e0e65ccc3057082d27e889c6c2dcab8726909eaf
tositeru <tositeru104@protonmail.com>
2020-04-24 11:02:09 +0900

    ViewInstanceCreatorフォルダーを追加

--- Runtime/MVC/ViewInstanceCreators/DefaultViewInstanceCreator.cs
+++ Runtime/MVC/ViewInstanceCreators/DefaultViewInstanceCreator.cs
@@ -7,50 +7,6 @@ using UnityEngine.Assertions;
 namespace Hinode
 {
     /// <summary>
-    /// IViewObjectとIModelViewParamBinderを作成する抽象クラス
-    /// </summary>
-    public abstract class IViewInstanceCreator
-    {
-        public System.Type GetViewObjType(ModelViewBinder.BindInfo bindInfo)
-        {
-            var type = GetViewObjTypeImpl(bindInfo.InstanceKey);
-            Assert.IsTrue(type.DoHasInterface<IViewObject>(), $"'{type.FullName}' don't have IViewObejct interface...");
-            return type;
-        }
-
-        public System.Type GetParamBinderType(ModelViewBinder.BindInfo bindInfo)
-        {
-            return GetParamBinder(bindInfo).GetType();
-        }
-
-        public IViewObject CreateViewObj(ModelViewBinder.BindInfo bindInfo)
-        {
-            var viewObj = CreateViewObjImpl(bindInfo.InstanceKey);
-            Assert.IsNotNull(viewObj, $"Failed to create ViewObject because don't match ViewObject Key({bindInfo.InstanceKey})...");
-            viewObj.UseBindInfo = bindInfo;
-            return viewObj;
-        }
-
-        public IModelViewParamBinder GetParamBinder(ModelViewBinder.BindInfo bindInfo)
-        {
-            if(bindInfo.UseParamBinder != null)
-            {
-                return bindInfo.UseParamBinder;
-            }
-            else
-            {
-                var paramBinder = GetParamBinderImpl(bindInfo.BinderKey);
-                Assert.IsNotNull(paramBinder, $"Failed to create IModelViewParamBinder because don't match Binder Key({paramBinder})...");
-                return paramBinder;
-            }
-        }
-
-        protected abstract System.Type GetViewObjTypeImpl(string instanceKey);
-        protected abstract IViewObject CreateViewObjImpl(string instanceKey);
-        protected abstract IModelViewParamBinder GetParamBinderImpl(string binderKey);
-    }
-
-    /// <summary>
     /// 汎用のModelViewBinder.IViewInstanceCreator
     ///
     /// 引数無しのコンストラクを持つIViewBinderObject型に対応しています
@@ -100,5 +56,4 @@ namespace Hinode
             return _dict[binderKey].paramBinder;
         }
     }
-
 }
---
+++ Runtime/MVC/ViewInstanceCreators/IViewInstanceCreator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Hinode
+{
+    /// <summary>
+    /// IViewObjectとIModelViewParamBinderを作成する抽象クラス
+    /// </summary>
+    public abstract class IViewInstanceCreator
+    {
+        public System.Type GetViewObjType(ModelViewBinder.BindInfo bindInfo)
+        {
+            var type = GetViewObjTypeImpl(bindInfo.InstanceKey);
+            Assert.IsTrue(type.DoHasInterface<IViewObject>(), $"'{type.FullName}' don't have IViewObejct interface...");
+            return type;
+        }
+
+        public System.Type GetParamBinderType(ModelViewBinder.BindInfo bindInfo)
+        {
+            return GetParamBinder(bindInfo).GetType();
+        }
+
+        public IViewObject CreateViewObj(ModelViewBinder.BindInfo bindInfo)
+        {
+            var viewObj = CreateViewObjImpl(bindInfo.InstanceKey);
+            Assert.IsNotNull(viewObj, $"Failed to create ViewObject because don't match ViewObject Key({bindInfo.InstanceKey})...");
+            viewObj.UseBindInfo = bindInfo;
+            return viewObj;
+        }
+
+        public IModelViewParamBinder GetParamBinder(ModelViewBinder.BindInfo bindInfo)
+        {
+            if(bindInfo.UseParamBinder != null)
+            {
+                return bindInfo.UseParamBinder;
+            }
+            else
+            {
+                var paramBinder = GetParamBinderImpl(bindInfo.BinderKey);
+                Assert.IsNotNull(paramBinder, $"Failed to create IModelViewParamBinder because don't match Binder Key({paramBinder})...");
+                return paramBinder;
+            }
+        }
+
+        protected abstract System.Type GetViewObjTypeImpl(string instanceKey);
+        protected abstract IViewObject CreateViewObjImpl(string instanceKey);
+        protected abstract IModelViewParamBinder GetParamBinderImpl(string binderKey);
+    }
+}
