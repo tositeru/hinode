2e520a7e271edc7cfcde7ba36054d30ce8cc80d8
tositeru <tositeru104@protonmail.com>
2020-06-24 18:06:11 +0900

    KeyboardFrameInputDataの追加

---
+++ Runtime/Input/FrameInputData/KeyboardFrameInputData.cs
@@ -0,0 +1,260 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using Hinode.Serialization;
+using UnityEngine;
+
+namespace Hinode
+{
+    /// <summary>
+    /// UnityEngine.EventSystemの1フレームにおける入力データを記録するためのもの
+    /// 一つ前と変化がないデータはシリアライズの対象にならないようになっています。
+    ///
+    /// 以下のKeyCodeは他のKeyCodeと同じものと扱います。
+    ///     KeyCode.LeftApple => KeyCode.LeftCommand
+    ///     KeyCode.RightApple => KeyCode.RightCommand
+    /// </summary>
+    [System.Serializable, HasKeyAndTypeDictionaryGetter(typeof(KeyboardFrameInputData))]
+    public class KeyboardFrameInputData : InputRecorder.IFrameDataRecorder
+        , ISerializable
+        , FrameInputData.IChildFrameInputData
+    {
+        public static IEnumerable<KeyCode> AllKeyCodes { get; } = System.Enum.GetValues(typeof(KeyCode))
+            .OfType<KeyCode>();
+
+        Dictionary<KeyCode, UpdateObserver<InputDefines.ButtonCondition>> _keyButtonsDict = new Dictionary<KeyCode, UpdateObserver<InputDefines.ButtonCondition>>();
+
+        HashSet<KeyCode> _enabledKeyCodeFilter = new HashSet<KeyCode>();
+
+        JsonSerializer _jsonSerializer = new JsonSerializer();
+
+        KeyCode TransformKeyCode(KeyCode keyCode)
+        {
+            switch(keyCode)
+            {
+                case KeyCode.LeftApple: return KeyCode.LeftCommand;
+                case KeyCode.RightApple: return KeyCode.RightApple;
+                default: return keyCode;
+            }
+        }
+
+        public void SetKeyButton(KeyCode keyCode, InputDefines.ButtonCondition condition)
+        {
+            if (!EnabledKeyCodes.Contains(keyCode)) return;
+
+            if (!_keyButtonsDict.ContainsKey(keyCode))
+            {
+                // InputDefines.ButtonCondition.Free must be InputDefines.ButtonCondition.Free for IUpdateObserver.DidUpdated!
+                var initialCondition = InputDefines.ButtonCondition.Free;
+                _keyButtonsDict.Add(keyCode, new UpdateObserver<InputDefines.ButtonCondition>(initialCondition));
+            }
+            _keyButtonsDict[keyCode].Value = condition;
+        }
+
+        public InputDefines.ButtonCondition GetKeyButton(KeyCode keyCode)
+        {
+            if (!EnabledKeyCodes.Contains(keyCode)) return InputDefines.ButtonCondition.Free;
+
+            keyCode = TransformKeyCode(keyCode);
+            return _keyButtonsDict.ContainsKey(keyCode)
+                ? _keyButtonsDict[keyCode].Value
+                : InputDefines.ButtonCondition.Free;
+        }
+
+        public IEnumerable<(KeyCode keyCode, UpdateObserver<InputDefines.ButtonCondition> observer)> KeyButtons
+        {
+            get => _keyButtonsDict
+                .Where(_t => EnabledKeyCodes.Contains(_t.Key))
+                .Select(_t => (keyCode: _t.Key, observer: _t.Value));
+        }
+
+        public KeyboardFrameInputData()
+        {
+        }
+
+        #region Key Filter
+        public void AddEnabledKeyCode(IEnumerable<KeyCode> enableKeyCodes)
+        {
+            foreach(var k in enableKeyCodes
+                .Where(_k => !_enabledKeyCodeFilter.Contains(_k)))
+            {
+                _enabledKeyCodeFilter.Add(k);
+            }
+        }
+        public void AddEnabledKeyCode(params KeyCode[] enableKeyCodes)
+            => AddEnabledKeyCode(enableKeyCodes.AsEnumerable());
+
+        public void RemoveEnabledKeyCode(IEnumerable<KeyCode> keyCodes)
+        {
+            foreach (var k in keyCodes
+                .Where(_k => _enabledKeyCodeFilter.Contains(_k)))
+            {
+                _enabledKeyCodeFilter.Remove(k);
+            }
+        }
+        public void RemoveEnabledKeyCode(params KeyCode[] enableKeyCodes)
+            => RemoveEnabledKeyCode(enableKeyCodes.AsEnumerable());
+
+        public IEnumerable<KeyCode> EnabledKeyCodes
+        {
+            get => _enabledKeyCodeFilter.Any()
+                ? _enabledKeyCodeFilter
+                : AllKeyCodes;
+        }
+        #endregion
+
+
+        public void CopyUpdatedDatasTo(KeyboardFrameInputData other)
+        {
+            foreach (var (keyCode, condition) in KeyButtons
+                .Where(_t => _t.observer.DidUpdated))
+            {
+                other.SetKeyButton(keyCode, condition.Value);
+            }
+        }
+
+        #region FrameInputData.IChildFrameInputData interface
+        public void ResetInputDatas()
+        {
+            foreach (var (key, observer) in KeyButtons)
+            {
+                observer.Reset();
+            }
+        }
+
+        public void UpdateInputDatas(ReplayableInput input)
+        {
+            ResetInputDatas();
+
+            if (input.AnyKey)
+            {
+                foreach (var keyCode in EnabledKeyCodes)
+                {
+                    var condition = input.GetKeyDown(keyCode)
+                        ? InputDefines.ButtonCondition.Down
+                        : input.GetKeyUp(keyCode)
+                            ? InputDefines.ButtonCondition.Up
+                            : input.GetKey(keyCode)
+                                ? InputDefines.ButtonCondition.Push
+                                : InputDefines.ButtonCondition.Free;
+                    SetKeyButton(keyCode, condition);
+                }
+            }
+            else
+            {
+                foreach (var (keyCode, observer) in KeyButtons
+                    .Where(_t => _t.observer.Value == InputDefines.ButtonCondition.Free))
+                {
+                    observer.Value = InputDefines.ButtonCondition.Free;
+                }
+            }
+        }
+
+        public IEnumerable<FrameInputData.KeyValue> GetValuesEnumerable()
+        {
+            return new ValuesEnumerable(this);
+        }
+
+        class ValuesEnumerable : IEnumerable<FrameInputData.KeyValue>
+            , IEnumerable
+        {
+            KeyboardFrameInputData _target;
+            public ValuesEnumerable(KeyboardFrameInputData target)
+            {
+                _target = target;
+            }
+
+            public IEnumerator<FrameInputData.KeyValue> GetEnumerator()
+                => _target.KeyButtons
+                .Select(_t => new FrameInputData.KeyValue(((int)_t.keyCode).ToString(), _t.observer))
+                .GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+        }
+
+        #endregion
+
+        #region InputRecorder.IFrameDataRecorder interface
+        /// <summary>
+        /// <see cref="InputRecorder.IFrameDataRecorder.ResetDatas()"/>
+        /// </summary>
+        public void ResetDatas()
+        {
+            _keyButtonsDict.Clear();
+        }
+
+        /// <summary>
+        /// <see cref="InputRecorder.IFrameDataRecorder.RecoverFrame(ReplayableInput, InputRecord.Frame)"/>
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="frame"></param>
+        public void RecoverFrame(ReplayableInput input, InputRecord.Frame frame)
+        {
+            var recoverInput = _jsonSerializer.Deserialize<KeyboardFrameInputData>(frame.InputText);
+
+            recoverInput.CopyUpdatedDatasTo(this);
+
+            foreach(var keyCode in AllKeyCodes)
+            {
+                input.SetRecordedKeyButton(keyCode, GetKeyButton(keyCode));
+            }
+        }
+
+        /// <summary>
+        /// <see cref="InputRecorder.IFrameDataRecorder.Update(ReplayableInput)"/>
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public InputRecord.Frame Update(ReplayableInput input)
+        {
+            UpdateInputDatas(input);
+
+            var frame = new InputRecord.Frame();
+            frame.InputText = _jsonSerializer.Serialize(this);
+            return frame;
+        }
+        #endregion
+
+        #region ISerializable interface
+        public KeyboardFrameInputData(SerializationInfo info, StreamingContext context)
+        {
+            var e = info.GetEnumerator();
+            while (e.MoveNext())
+            {
+                if(int.TryParse(e.Name, out var keyCodeInt))
+                {
+                    var keyCode = (KeyCode)keyCodeInt;
+                    SetKeyButton(keyCode, (InputDefines.ButtonCondition)e.Value);
+                }
+            }
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            foreach(var (keyCode, observer) in KeyButtons
+                .Where(_t => _t.observer.DidUpdated))
+            {
+                info.AddValue(((int)keyCode).ToString(), (int)observer.Value);
+            }
+        }
+        #endregion
+
+        static Dictionary<string, System.Type> _keyAndTypeDict;
+        [KeyAndTypeDictionaryGetter]
+        public static IReadOnlyDictionary<string, System.Type> GetKeyAndTypeDictionary()
+        {
+            if (_keyAndTypeDict == null)
+            {
+                _keyAndTypeDict = new Dictionary<string, System.Type>();
+                foreach (var keyCode in AllKeyCodes
+                    .Where(_k => !_keyAndTypeDict.ContainsKey(((int)_k).ToString())))
+                {
+                    _keyAndTypeDict.Add(((int)keyCode).ToString(), typeof(int));
+                }
+            }
+            return _keyAndTypeDict;
+        }
+
+    }
+}
---
+++ Tests/Runtime/Input/FrameInputData/TestKeyboardFrameInputData.cs
@@ -0,0 +1,409 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Hinode.Serialization;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Hinode.Tests.Input.FrameInputData
+{
+    /// <summary>
+    /// <seealso cref="KeyboardFrameInputData"/>
+    /// </summary>
+    public class TestKeyboardFrameInputData
+    {
+        /// <summary>
+        /// <see cref="KeyboardFrameInputData.GetKeyButton(KeyCode)"/>
+        /// <see cref="KeyboardFrameInputData.SetKeyButton(KeyCode, InputDefines.ButtonCondition)"/>
+        /// <see cref="KeyboardFrameInputData.KeyButtons"/>
+        /// </summary>
+        [Test]
+        public void KeyButtonsPasses()
+        {
+            var data = new KeyboardFrameInputData();
+
+            foreach(var keyCode in System.Enum.GetValues(typeof(KeyCode)).OfType<KeyCode>())
+            {
+                var errorMessage = $"Failed KeyButton... KeyCode={keyCode}";
+                Assert.AreEqual(InputDefines.ButtonCondition.Free, data.GetKeyButton(keyCode), errorMessage);
+
+                //Debug.Log($"Success to not Set yet to Button({keyCode})!");
+
+                data.SetKeyButton(keyCode, InputDefines.ButtonCondition.Push);
+                Assert.AreEqual(InputDefines.ButtonCondition.Push, data.GetKeyButton(keyCode), errorMessage);
+                Assert.IsTrue(data.KeyButtons.Any(_t => _t.keyCode == keyCode), errorMessage);
+
+                //Debug.Log($"Success to Set to Button({keyCode})!");
+
+                data.SetKeyButton(keyCode, InputDefines.ButtonCondition.Free);
+                Assert.IsTrue(data.KeyButtons.Any(_t => _t.keyCode == keyCode), errorMessage);
+
+                //Debug.Log($"Success to Set ButtonCondition.Free to Button({keyCode})!");
+            }
+        }
+
+        /// <summary>
+        /// <seealso cref="KeyboardFrameInputData.SetKeyButton(KeyCode, InputDefines.ButtonCondition)"/>
+        /// <seealso cref="KeyboardFrameInputData.GetKeyButton(KeyCode)"/>
+        /// </summary>
+        [Test]
+        public void TransformedKeyCodePasses()
+        {
+            var data = new KeyboardFrameInputData();
+
+            var testData = new (KeyCode use, KeyCode transform)[] {
+                (KeyCode.LeftApple, KeyCode.LeftCommand),
+                (KeyCode.RightApple, KeyCode.RightCommand),
+            };
+
+            foreach(var d in testData)
+            {
+                var errorMessage = $"keyCode={d.use}, transformed={d.transform}";
+                data.SetKeyButton(d.use, InputDefines.ButtonCondition.Push);
+                Assert.AreEqual(data.GetKeyButton(d.transform), data.GetKeyButton(d.use), errorMessage);
+
+                data.SetKeyButton(d.use, InputDefines.ButtonCondition.Down);
+                Assert.AreEqual(data.GetKeyButton(d.transform), data.GetKeyButton(d.use), errorMessage);
+
+                data.SetKeyButton(d.use, InputDefines.ButtonCondition.Free);
+                Assert.AreEqual(data.GetKeyButton(d.transform), data.GetKeyButton(d.use), errorMessage);
+            }
+        }
+
+        /// <summary>
+        /// <seealso cref="KeyboardFrameInputData.AddEnabledKeyCode(IEnumerable{KeyCode})"/>
+        /// <seealso cref="KeyboardFrameInputData.EnabledKeyCodes"/>
+        /// </summary>
+        [Test]
+        public void AddKeyFilterPasses()
+        {
+            var data = new KeyboardFrameInputData();
+            AssertionUtils.AssertEnumerableByUnordered(KeyboardFrameInputData.AllKeyCodes, data.EnabledKeyCodes, "");
+            Debug.Log($"Success Initial EnabledKeyCodes!");
+
+            var enableKeyCodes = new KeyCode[]
+            {
+                KeyCode.A,
+                KeyCode.LeftCommand,
+                KeyCode.At,
+                KeyCode.F2,
+                KeyCode.Keypad2,
+            };
+            data.AddEnabledKeyCode(enableKeyCodes);
+            AssertionUtils.AssertEnumerableByUnordered(enableKeyCodes, data.EnabledKeyCodes, "");
+            Debug.Log($"Success to SetEnabledKeyCode!");
+
+            var allKeyCodes = KeyboardFrameInputData.AllKeyCodes;
+            foreach(var keyCode in allKeyCodes)
+            {
+                var condition = InputDefines.ButtonCondition.Push;
+                data.SetKeyButton(keyCode, condition);
+
+                if(enableKeyCodes.Contains(keyCode))
+                {
+                    Assert.AreEqual(condition, data.GetKeyButton(keyCode), $"Fail Filtering KeyCode({keyCode})...");
+                }
+                else
+                {
+                    Assert.AreNotEqual(condition, data.GetKeyButton(keyCode), $"Fail None Filtering KeyCode({keyCode})...");
+                }
+            }
+            Debug.Log($"Success to Filter EnabledKeyCode!");
+
+            AssertionUtils.AssertEnumerableByUnordered(data.EnabledKeyCodes, data.KeyButtons.Select(_t => _t.keyCode), "");
+            Debug.Log($"Success to Filtering KeyButtons!");
+        }
+
+        /// <summary>
+        /// <seealso cref="KeyboardFrameInputData.RemoveEnabledKeyCode(IEnumerable{KeyCode})"/>
+        /// <seealso cref="KeyboardFrameInputData.EnabledKeyCodes"/>
+        /// </summary>
+        [Test]
+        public void RemoveKeyFilterPasses()
+        {
+            var data = new KeyboardFrameInputData();
+
+            var enableKeyCodes = new KeyCode[]
+            {
+                KeyCode.A,
+                KeyCode.LeftCommand,
+                KeyCode.At,
+                KeyCode.F2,
+                KeyCode.Keypad2,
+            };
+            data.AddEnabledKeyCode(enableKeyCodes);
+
+            var removeKeyCodes = new KeyCode[]
+            {
+                KeyCode.A, KeyCode.B, KeyCode.At
+            };
+            data.RemoveEnabledKeyCode(removeKeyCodes);
+
+            var allKeyCodes = System.Enum.GetValues(typeof(KeyCode)).OfType<KeyCode>();
+
+            foreach (var keyCode in allKeyCodes)
+            {
+                var condition = InputDefines.ButtonCondition.Push;
+                data.SetKeyButton(keyCode, condition);
+
+                if (enableKeyCodes.Contains(keyCode) && !removeKeyCodes.Contains(keyCode))
+                {
+                    Assert.AreEqual(condition, data.GetKeyButton(keyCode), $"Fail Filtering KeyCode({keyCode})...");
+                }
+                else
+                {
+                    Assert.AreNotEqual(condition, data.GetKeyButton(keyCode), $"Fail None Filtering KeyCode({keyCode})...");
+                }
+            }
+        }
+
+        /// <summary>
+        /// <seealso cref="KeyboardFrameInputData.GetValuesEnumerable()"/>
+        /// </summary>
+        [Test, Description("KeyButton only Get Not ButtonCondition.Free!")]
+        public void GetValuesEnumerablePasses()
+        {
+            var data = new KeyboardFrameInputData();
+            var testData = new (KeyCode keyCode, InputDefines.ButtonCondition condition)[]
+            {
+                (KeyCode.A, InputDefines.ButtonCondition.Push),
+                (KeyCode.Keypad3, InputDefines.ButtonCondition.Down),
+                (KeyCode.JoystickButton8, InputDefines.ButtonCondition.Up),
+                (KeyCode.F1, InputDefines.ButtonCondition.Down),
+                (KeyCode.LeftShift, InputDefines.ButtonCondition.Push),
+            };
+
+            foreach(var (keyCode, condition) in testData)
+            {
+                data.SetKeyButton(keyCode, condition);
+            }
+
+            AssertionUtils.AssertEnumerableByUnordered(
+                testData.Select(_t => (((int)_t.keyCode).ToString(), (object)_t.condition))
+                , data.GetValuesEnumerable().Select(_t => (_t.Key, _t.Value.RawValue))
+                , "Don't match GetValuesEnumerable()... only Not ButtonCondition.Free KeyCode..."
+            );
+        }
+
+        /// <summary>
+        /// <seealso cref="KeyboardFrameInputData.GetValuesEnumerable()"/>
+        /// <seealso cref="KeyboardFrameInputData.GetKeyAndTypeDictionary()"/>
+        /// </summary>
+        [Test]
+        public void GetKeyAndTypeDictionaryPasses()
+        {
+            var data = new KeyboardFrameInputData();
+
+            var allKeyCodes = System.Enum.GetValues(typeof(KeyCode))
+                .OfType<KeyCode>();
+            AssertionUtils.AssertEnumerableByUnordered(
+                allKeyCodes.Select(_k => (((int)_k).ToString(), typeof(int)))
+                    .Distinct()
+                , KeyboardFrameInputData.GetKeyAndTypeDictionary()
+                    .Select(_t => (_t.Key, _t.Value))
+                , "Don't match key and Type..."
+            );
+        }
+
+        /// <summary>
+        /// <seealso cref="KeyboardFrameInputData.UpdateInputDatas(ReplayableInput)"/>
+        /// </summary>
+        [Test]
+        public void UpdateInputDatasPasses()
+        {
+            //好きなデータを指定できるためReplayableInputを使用している
+            var replayInput = ReplayableInput.Instance;
+            replayInput.IsReplaying = true;
+
+            var allKeyCodes = System.Enum.GetValues(typeof(KeyCode)).OfType<KeyCode>();
+            foreach (var keyCode in allKeyCodes)
+            {
+                var n = (int)keyCode % 5;
+                if(n < 4)
+                {
+                    var condition = (InputDefines.ButtonCondition)(n);
+                    replayInput.SetRecordedKeyButton(keyCode, condition);
+                }
+            }
+
+            //データが正しく設定されるか確認
+            var data = new KeyboardFrameInputData();
+            data.UpdateInputDatas(replayInput);
+
+            foreach(var keyCode in allKeyCodes)
+            {
+                Assert.AreEqual(replayInput.GetRecordedKeyButton(keyCode), data.GetKeyButton(keyCode), $"Fail KeyCode({keyCode})");
+            }
+        }
+
+        /// <summary>
+        /// シリアライズも含めたデータ更新処理が想定しているように動作しているか確認するテスト
+        /// <seealso cref="KeyboardFrameInputData.GetValuesEnumerable()"/>
+        /// <seealso cref="KeyboardFrameInputData.KeyboardFrameInputData(System.Runtime.Serialization.SerializationInfo, System.Runtime.Serialization.StreamingContext)"/>
+        /// <seealso cref="KeyboardFrameInputData.GetObjectData(System.Runtime.Serialization.SerializationInfo, System.Runtime.Serialization.StreamingContext)"/>
+        /// </summary>
+        [Test]
+        public void SerializationPasses()
+        {
+            var data = new KeyboardFrameInputData();
+
+            var allKeyCodes = System.Enum.GetValues(typeof(KeyCode)).OfType<KeyCode>();
+            foreach (var keyCode in allKeyCodes)
+            {
+                var n = (int)keyCode % 5;
+                if (n < 4)
+                {
+                    var condition = (InputDefines.ButtonCondition)(n);
+                    data.SetKeyButton(keyCode, condition);
+                }
+            }
+
+            var serializer = new JsonSerializer();
+            var json = serializer.Serialize(data);
+            Debug.Log($"debug -- json:{json}");
+            var dest = serializer.Deserialize<KeyboardFrameInputData>(json);
+
+            {
+                var errorMessage = "Failed to serialize value Count...";
+                Assert.AreEqual(data.GetValuesEnumerable().Where(_t => _t.Value.DidUpdated).Count()
+                    , dest.GetValuesEnumerable().Where(_t => _t.Value.DidUpdated).Count(), errorMessage);
+            }
+            Debug.Log($"Success to Serialization Value Count(Only Updated)!");
+
+            {
+                foreach (var keyCode in allKeyCodes)
+                {
+                    Assert.AreEqual(data.GetKeyButton(keyCode), dest.GetKeyButton(keyCode), $"Failed to serialize Key Button({keyCode})...");
+                }
+            }
+            Debug.Log($"Success to Serialization Value(Only Updated)!");
+        }
+
+        /// <summary>
+        /// <seealso cref="KeyboardFrameInputData.Update(ReplayableInput)"/>
+        /// </summary>
+        [Test]
+        public void UpdatePasses()
+        {
+            //好きなデータを指定できるためReplayableInputを使用している
+            var replayInput = ReplayableInput.Instance;
+
+            replayInput.IsReplaying = true;
+
+            var allKeyCodes = System.Enum.GetValues(typeof(KeyCode)).OfType<KeyCode>();
+            foreach (var keyCode in allKeyCodes)
+            {
+                var n = (int)keyCode % 5;
+                if (n < 4)
+                {
+                    var condition = (InputDefines.ButtonCondition)(n);
+                    replayInput.SetRecordedKeyButton(keyCode, condition);
+                }
+            }
+
+            //データが正しく設定されるか確認
+            var data = new KeyboardFrameInputData();
+            var frame = data.Update(replayInput);
+            //Mouse
+            foreach (var keyCode in allKeyCodes)
+            {
+                Assert.AreEqual(replayInput.GetRecordedKeyButton(keyCode), data.GetKeyButton(keyCode));
+            }
+            Debug.Log($"Success to Update Input Datas!");
+
+            {
+                var jsonSerializer = new JsonSerializer();
+                var recoverInput = jsonSerializer.Deserialize<KeyboardFrameInputData>(frame.InputText);
+                AssertionUtils.AssertEnumerable(
+                    new (string, object)[] { }
+                    , recoverInput.GetValuesEnumerable().Select(_t => (_t.Key, _t.Value.RawValue))
+                    , "Don't match Input Values..."
+                );
+            }
+            Debug.Log($"Success to Create InputRecord.Frame!");
+
+        }
+
+        [Test]
+        public void RecoverFramePasses()
+        {
+            //好きなデータを指定できるためReplayableInputを使用している
+            var replayInput = ReplayableInput.Instance;
+
+            var data = new KeyboardFrameInputData();
+
+            var allKeyCodes = System.Enum.GetValues(typeof(KeyCode)).OfType<KeyCode>();
+            foreach (var keyCode in allKeyCodes)
+            {
+                var n = (int)keyCode % 5;
+                if (n < 4)
+                {
+                    var condition = (InputDefines.ButtonCondition)(n);
+                    data.SetKeyButton(keyCode, condition);
+                }
+            }
+
+            var frameData = new InputRecord.Frame();
+            var serializer = new JsonSerializer();
+            frameData.InputText = serializer.Serialize(data);
+
+            //データが正しく設定されるか確認
+            var recoveredData = new KeyboardFrameInputData();
+            recoveredData.RecoverFrame(replayInput, frameData);
+
+            AssertionUtils.AssertEnumerableByUnordered(
+                data.GetValuesEnumerable()
+                    .Where(_t => (InputDefines.ButtonCondition)_t.Value.RawValue != InputDefines.ButtonCondition.Free)
+                    .Select(_t => (_t.Key, _t.Value.RawValue))
+                , recoveredData.GetValuesEnumerable().Select(_t => (_t.Key, _t.Value.RawValue))
+                , "Failed to Recover Input Datas..."
+            );
+            Debug.Log($"Success to Recover Input Datas!");
+
+            {
+                var errorMessage = "Failed to Set Input Datas to ReplayableInput...";
+                foreach (var keyCode in allKeyCodes)
+                {
+                    Assert.AreEqual(data.GetKeyButton(keyCode), replayInput.GetRecordedKeyButton(keyCode), $"{errorMessage}... keyCode={keyCode}");
+                }
+            }
+            Debug.Log($"Success to Set Input Datas to ReplayableInput!");
+        }
+
+        /// <summary>
+        /// <see cref="KeyboardFrameInputData.ResetDatas()"/>
+        /// </summary>
+        [Test]
+        public void ResetDatasPasses()
+        {
+            //好きなデータを指定できるためReplayableInputを使用している
+            var replayInput = ReplayableInput.Instance;
+
+            replayInput.IsReplaying = true;
+
+            var allKeyCodes = System.Enum.GetValues(typeof(KeyCode)).OfType<KeyCode>();
+            foreach (var keyCode in allKeyCodes)
+            {
+                var n = (int)keyCode % 5;
+                if (n < 4)
+                {
+                    var condition = (InputDefines.ButtonCondition)(n);
+                    replayInput.SetRecordedKeyButton(keyCode, condition);
+                }
+            }
+
+            //データが正しく設定されるか確認
+            var data = new KeyboardFrameInputData();
+            data.UpdateInputDatas(replayInput);
+
+            data.ResetDatas();
+            Assert.IsTrue(data.GetValuesEnumerable().All(_t => !_t.Value.DidUpdated), "Failed to reset DidUpdated...");
+            AssertionUtils.AssertEnumerableByUnordered(
+                new (string key, object value)[] { },
+                data.GetValuesEnumerable().Select(_t => (_t.Key, _t.Value.RawValue))
+                , "Don't match GetValuesEnumerable()..."
+            );
+        }
+    }
+}
