882560fa88030b9d129bc21e8740bff34fd80d9f
tositeru <tositeru104@protonmail.com>
2020-06-26 09:54:29 +0900

    ButtonFrameInputDataを作成

---
+++ Runtime/Input/FrameInputData/ButtonFrameInputData.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using Hinode.Serialization;
+using UnityEngine;
+
+namespace Hinode
+{
+    /// <summary>
+    /// UnityEngine.EventSystemの1フレームにおける入力データを記録するためのもの
+    /// 一つ前と変化がないデータはシリアライズの対象にならないようになっています。
+    /// </summary>
+    [System.Serializable, ContainsSerializationKeyTypeGetter(typeof(ButtonFrameInputData))]
+    public class ButtonFrameInputData : InputRecorder.IFrameDataRecorder
+        , ISerializable
+        , FrameInputData.IChildFrameInputData
+    {
+        [SerializeField] Dictionary<string, UpdateObserver<InputDefines.ButtonCondition>> _buttons = new Dictionary<string, UpdateObserver<InputDefines.ButtonCondition>>();
+        readonly HashSet<string> _observedButtonNames = new HashSet<string>();
+
+        JsonSerializer _jsonSerializer = new JsonSerializer();
+
+        public IReadOnlyCollection<string> ObservedButtonNames { get => _observedButtonNames; }
+
+        public ButtonFrameInputData()
+        {
+        }
+
+        public bool ContainsButton(string name)
+            => _observedButtonNames.Contains(name);
+
+        #region Observed Button Name
+        public void AddObservedButtonNames(IEnumerable<string> buttonName)
+        {
+            foreach(var name in buttonName
+                .Where(_n => !_observedButtonNames.Contains(_n)))
+            {
+                _observedButtonNames.Add(name);
+
+                if (!_buttons.ContainsKey(name))
+                {
+                    var observer = new UpdateObserver<InputDefines.ButtonCondition>(InputDefines.ButtonCondition.Free);
+                    _buttons.Add(name, observer);
+                }
+            }
+        }
+        public void AddObservedButtonNames(params string[] buttonName)
+            => AddObservedButtonNames(buttonName.AsEnumerable());
+
+        public void RemoveObservedButtonNames(IEnumerable<string> buttonName)
+        {
+            foreach (var name in buttonName
+                .Where(_n => _observedButtonNames.Contains(_n)))
+            {
+                _observedButtonNames.Remove(name);
+
+                if (_buttons.ContainsKey(name))
+                {
+                    _buttons[name].Value = InputDefines.ButtonCondition.Free;
+                }
+            }
+        }
+        public void RemoveObservedButtonNames(params string[] buttonName)
+            => RemoveObservedButtonNames(buttonName.AsEnumerable());
+        #endregion
+
+        public InputDefines.ButtonCondition GetButton(string name)
+            => _buttons.ContainsKey(name)
+            ? _buttons[name].Value
+            : InputDefines.ButtonCondition.Free;
+
+        public void SetButton(string name, InputDefines.ButtonCondition condition)
+        {
+            if (!_buttons.ContainsKey(name)) return;
+
+            _buttons[name].Value = condition;
+        }
+
+        public void CopyUpdatedDatasTo(ButtonFrameInputData other)
+        {
+            foreach(var (name, observer) in _buttons
+                .Where(_b => _b.Value.DidUpdated)
+                .Select(_b => (name: _b.Key, observer: _b.Value)))
+            {
+                other.SetButton(name, observer.Value);
+            }
+        }
+
+        #region FrameInputData.IChildFrameInputData interface
+        public void ResetInputDatas()
+        {
+            foreach (var observer in _buttons
+                .Where(_b => _b.Value.DidUpdated)
+                .Select(_b => _b.Value))
+            {
+                observer.Reset();
+            }
+        }
+
+        public void UpdateInputDatas(ReplayableInput input)
+        {
+            ResetInputDatas();
+
+            foreach(var name in _observedButtonNames)
+            {
+                var condition = input.GetButtonCondition(name);
+                SetButton(name, condition);
+            }
+        }
+
+        public IEnumerable<FrameInputData.KeyValue> GetValuesEnumerable()
+        {
+            return new ValuesEnumerable(this);
+        }
+
+        class ValuesEnumerable : IEnumerable<FrameInputData.KeyValue>
+            , IEnumerable
+        {
+            ButtonFrameInputData _target;
+            public ValuesEnumerable(ButtonFrameInputData target)
+            {
+                _target = target;
+            }
+
+            public IEnumerator<FrameInputData.KeyValue> GetEnumerator()
+            {
+                foreach(var name in _target._observedButtonNames)
+                {
+                    yield return (name, _target._buttons[name]);
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+        }
+
+        #endregion
+
+        #region InputRecorder.IFrameDataRecorder interface
+        /// <summary>
+        /// <see cref="InputRecorder.IFrameDataRecorder.ResetDatas()"/>
+        /// </summary>
+        public void ResetDatas()
+        {
+            foreach(var btn in _buttons)
+            {
+                btn.Value.SetDefaultValue(true);
+            }
+        }
+
+        /// <summary>
+        /// <see cref="InputRecorder.IFrameDataRecorder.RecoverFrame(ReplayableInput, InputRecord.Frame)"/>
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="frame"></param>
+        public void RecoverFrame(ReplayableInput input, InputRecord.Frame frame)
+        {
+            var recoverInput = _jsonSerializer.Deserialize<ButtonFrameInputData>(frame.InputText);
+
+            recoverInput.CopyUpdatedDatasTo(this);
+
+            foreach(var (name, observer) in _buttons.Select(_t => (_t.Key, _t.Value)))
+            {
+                input.SetRecordedButton(name, observer.Value);
+            }
+        }
+
+        /// <summary>
+        /// <see cref="InputRecorder.IFrameDataRecorder.Update(ReplayableInput)"/>
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public InputRecord.Frame Update(ReplayableInput input)
+        {
+            UpdateInputDatas(input);
+
+            var frame = new InputRecord.Frame();
+            frame.InputText = _jsonSerializer.Serialize(this);
+            return frame;
+        }
+        #endregion
+
+        #region ISerializable interface
+
+        public ButtonFrameInputData(SerializationInfo info, StreamingContext context)
+        {
+            var e = info.GetEnumerator();
+            while (e.MoveNext())
+            {
+                AddObservedButtonNames(e.Name);
+                SetButton(e.Name, (InputDefines.ButtonCondition)e.Value);
+            }
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            foreach(var (name, observer) in _buttons.Select(_t => (_t.Key, _t.Value)))
+            {
+                info.AddValue(name, (int)observer.Value);
+            }
+        }
+        #endregion
+
+        [SerializationKeyTypeGetter]
+        public static System.Type GetKeyType(string key)
+        {
+            return typeof(int);
+        }
+
+    }
+}
---
+++ Tests/Runtime/Input/FrameInputData/TestButtonFrameInputData.cs
@@ -0,0 +1,296 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Hinode.Serialization;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Hinode.Tests.Input.FrameInputData
+{
+    /// <summary>
+    /// <seealso cref="ButtonFrameInputData"/>
+    /// </summary>
+    public class TestButtonFrameInputData
+    {
+        /// <summary>
+        /// </summary>
+        [Test]
+        public void ButtonBasicUsagePasses()
+        {
+            var data = new ButtonFrameInputData();
+            var buttonName = "Fire1";
+            data.AddObservedButtonNames(buttonName);
+            Assert.IsTrue(data.ContainsButton(buttonName));
+
+            data.SetButton(buttonName, InputDefines.ButtonCondition.Down);
+            Assert.AreEqual(InputDefines.ButtonCondition.Down, data.GetButton(buttonName));
+
+        }
+
+        /// <summary>
+        /// <seealso cref="ButtonFrameInputData.GetValuesEnumerable()"/>
+        /// </summary>
+        [Test]
+        public void GetValuesEnumerablePasses()
+        {
+            var data = new ButtonFrameInputData();
+            var buttonNames = new string[] {
+                "Fire1",
+                "Fire2",
+                "Jump",
+            };
+            data.AddObservedButtonNames(buttonNames);
+
+            AssertionUtils.AssertEnumerableByUnordered(
+                buttonNames.Select(_n => (key: _n, (object)data.GetButton(_n)))
+                , data.GetValuesEnumerable().Select(_t => (_t.Key, _t.Value.RawValue))
+                , "Don't match GetValuesEnumerable()..."
+            );
+        }
+
+        /// <summary>
+        /// <seealso cref="ButtonFrameInputData.GetValuesEnumerable()"/>
+        /// <seealso cref="ButtonFrameInputData.GetKeyType(string)"/>
+        /// </summary>
+        [Test]
+        public void GetKeyTypePasses()
+        {
+            var testData = new (string key, System.Type type)[]
+            {
+                ("Fire1", typeof(int)),
+                ("Fire2", typeof(int)),
+                ("Jump", typeof(int)),
+            };
+            foreach (var d in testData)
+            {
+                var errorMessage = $"Don't match key and Type... key={d.key}";
+                Assert.AreEqual(d.type, ButtonFrameInputData.GetKeyType(d.key), errorMessage);
+            }
+        }
+
+        /// <summary>
+        /// <seealso cref="ButtonFrameInputData.UpdateInputDatas(ReplayableInput)"/>
+        /// </summary>
+        [Test]
+        public void UpdateInputDatasPasses()
+        {
+            //好きなデータを指定できるためReplayableInputを使用している
+            var replayInput = ReplayableInput.Instance;
+
+            replayInput.IsReplaying = true;
+            var buttonNames = new string[] {
+                "Fire1",
+                "Fire2",
+                "Jump",
+            };
+            foreach(var name in buttonNames)
+            {
+                replayInput.SetRecordedButton(name, InputDefines.ButtonCondition.Down);
+            }
+
+            //データが正しく設定されるか確認
+            var data = new ButtonFrameInputData();
+            var observedButtonNames = new string[] {
+                "Fire1",
+                "Jump",
+            };
+            data.AddObservedButtonNames(observedButtonNames);
+
+            //Update only observed Button
+            data.UpdateInputDatas(replayInput);
+
+            foreach (var name in buttonNames)
+            {
+                var errorMessage = $"Fail... ButtonName={name}";
+                if (observedButtonNames.Contains(name))
+                {
+                    Assert.AreEqual(replayInput.GetButtonCondition(name), data.GetButton(name), errorMessage);
+                }
+                else
+                {
+                    Assert.AreEqual(InputDefines.ButtonCondition.Free, data.GetButton(name), errorMessage);
+                }
+            }
+        }
+
+        /// <summary>
+        /// シリアライズも含めたデータ更新処理が想定しているように動作しているか確認するテスト
+        /// <seealso cref="ButtonFrameInputData.GetValuesEnumerable()"/>
+        /// <seealso cref="ButtonFrameInputData.ButtonFrameInputData(System.Runtime.Serialization.SerializationInfo, System.Runtime.Serialization.StreamingContext)"/>
+        /// <seealso cref="ButtonFrameInputData.GetObjectData(System.Runtime.Serialization.SerializationInfo, System.Runtime.Serialization.StreamingContext)"/>
+        /// </summary>
+        [Test]
+        public void SerializationPasses()
+        {
+            var data = new ButtonFrameInputData();
+
+            var buttonNames = new string[] {
+                "Fire1",
+                "Fire2",
+                "Jump",
+            };
+            foreach (var name in buttonNames)
+            {
+                data.SetButton(name, InputDefines.ButtonCondition.Down);
+            }
+
+            var serializer = new JsonSerializer();
+            var json = serializer.Serialize(data);
+            Debug.Log($"debug -- json:{json}");
+            var dest = serializer.Deserialize<ButtonFrameInputData>(json);
+
+            {
+                var errorMessage = "Failed to serialize value Count...";
+                Assert.AreEqual(data.GetValuesEnumerable().Where(_t => _t.Value.DidUpdated).Count()
+                    , dest.GetValuesEnumerable().Where(_t => _t.Value.DidUpdated).Count(), errorMessage);
+            }
+            Debug.Log($"Success to Serialization Value Count(Only Updated)!");
+
+            {
+                foreach(var buttonName in data.ObservedButtonNames)
+                {
+                    Assert.AreEqual(data.GetButton(buttonName), dest.GetButton(buttonName), $"Failed to serialize Button({buttonName})...");
+                }
+            }
+            Debug.Log($"Success to Serialization Value(Only Updated)!");
+        }
+
+        /// <summary>
+        /// <seealso cref="ButtonFrameInputData.Update(ReplayableInput)"/>
+        /// </summary>
+        [Test]
+        public void UpdatePasses()
+        {
+            //好きなデータを指定できるためReplayableInputを使用している
+            var replayInput = ReplayableInput.Instance;
+
+            replayInput.IsReplaying = true;
+            var buttonNames = new string[] {
+                "Fire1",
+                "Fire2",
+                "Jump",
+            };
+            foreach (var name in buttonNames)
+            {
+                replayInput.SetRecordedButton(name, InputDefines.ButtonCondition.Down);
+            }
+
+            //データが正しく設定されるか確認
+            var data = new ButtonFrameInputData();
+            var observedButtonNames = new string[] {
+                "Fire2",
+                "Jump",
+            };
+            data.AddObservedButtonNames(observedButtonNames);
+
+            var frame = data.Update(replayInput);
+
+            foreach (var name in buttonNames)
+            {
+                var errorMessage = $"Fail... ButtonName={name}";
+                if (observedButtonNames.Contains(name))
+                {
+                    Assert.AreEqual(replayInput.GetButtonCondition(name), data.GetButton(name), errorMessage);
+                }
+                else
+                {
+                    Assert.AreEqual(InputDefines.ButtonCondition.Free, data.GetButton(name), errorMessage);
+                }
+            }
+            Debug.Log($"Success to Update Input Datas!");
+
+            {
+                var jsonSerializer = new JsonSerializer();
+                var recoverInput = jsonSerializer.Deserialize<ButtonFrameInputData>(frame.InputText);
+                AssertionUtils.AssertEnumerable(
+                    data.GetValuesEnumerable().Select(_t => (_t.Key, _t.Value.RawValue))
+                    , recoverInput.GetValuesEnumerable().Select(_t => (_t.Key, _t.Value.RawValue))
+                    , "Don't match Input Values..."
+                );
+            }
+            Debug.Log($"Success to Create InputRecord.Frame!");
+        }
+
+        [Test]
+        public void RecoverFramePasses()
+        {
+            //好きなデータを指定できるためReplayableInputを使用している
+            var replayInput = ReplayableInput.Instance;
+
+            var data = new ButtonFrameInputData();
+            var buttonNames = new string[] {
+                "Fire1",
+                "Fire2",
+                "Jump",
+            };
+            foreach (var name in buttonNames)
+            {
+                data.SetButton(name, InputDefines.ButtonCondition.Down);
+            }
+
+            var frameData = new InputRecord.Frame();
+            var serializer = new JsonSerializer();
+            frameData.InputText = serializer.Serialize(data);
+
+            //データが正しく設定されるか確認
+            var recoveredData = new ButtonFrameInputData();
+            recoveredData.RecoverFrame(replayInput, frameData);
+
+            AssertionUtils.AssertEnumerable(
+                data.GetValuesEnumerable().Select(_t => (_t.Key, _t.Value.RawValue))
+                , recoveredData.GetValuesEnumerable().Select(_t => (_t.Key, _t.Value.RawValue))
+                , "Failed to Recover Input Datas..."
+            );
+            Debug.Log($"Success to Recover Input Datas!");
+
+            {
+                foreach (var buttonName in data.ObservedButtonNames)
+                {
+                    Assert.AreEqual(data.GetButton(buttonName), replayInput.GetButton(buttonName), $"Failed to serialize Button({buttonName})...");
+                }
+            }
+            Debug.Log($"Success to Set Input Datas to ReplayableInput!");
+        }
+
+        /// <summary>
+        /// <see cref="ButtonFrameInputData.ResetDatas()"/>
+        /// </summary>
+        [Test]
+        public void ResetDatasPasses()
+        {
+            //好きなデータを指定できるためReplayableInputを使用している
+            var replayInput = ReplayableInput.Instance;
+
+            replayInput.IsReplaying = true;
+
+            var buttonNames = new string[] {
+                "Fire1",
+                "Fire2",
+                "Jump",
+            };
+            foreach (var name in buttonNames)
+            {
+                replayInput.SetRecordedButton(name, InputDefines.ButtonCondition.Down);
+            }
+
+            //データが正しく設定されるか確認
+            var data = new ButtonFrameInputData();
+            var observedButtonNames = new string[] {
+                "Fire1",
+                "Jump",
+            };
+            data.AddObservedButtonNames(observedButtonNames);
+
+            data.UpdateInputDatas(replayInput);
+
+            data.ResetDatas();
+            Assert.IsTrue(data.GetValuesEnumerable().All(_t => !_t.Value.DidUpdated), "Failed to reset DidUpdated...");
+            AssertionUtils.AssertEnumerableByUnordered(
+                data.ObservedButtonNames.Select(_n => (key: _n, value: (object)default(InputDefines.ButtonCondition)))
+                , data.GetValuesEnumerable().Select(_t => (_t.Key, _t.Value.RawValue))
+                , "Don't match GetValuesEnumerable()..."
+            );
+        }
+    }
+}
