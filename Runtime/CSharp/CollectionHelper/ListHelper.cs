093f6da19522fccb3c180c23a08c8ea51dc47fd1
tositeru <tositeru104@protonmail.com>
2020-07-14 11:12:02 +0900

    namespace Hinode.CollectionHelperを削除 (#6)

--- Runtime/CSharp/CollectionHelper/ListHelper.cs
+++ Runtime/CSharp/CollectionHelper/ListHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-namespace Hinode.CollectionHelper
+namespace Hinode
 {
     public static class ListHelperCallback<T>
     {
