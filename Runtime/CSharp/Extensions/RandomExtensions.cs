51e460d246d98b0d04e2e2dc27bfa5e9b20f0cc2
tositeru <tositeru104@protonmail.com>
2020-07-25 17:09:57 +0900

    add RandomExtensions

---
+++ Runtime/CSharp/Extensions/RandomExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hinode
+{
+    public static partial class RandomExtensions
+    {
+        /// <summary>
+		/// 
+		/// </summary>
+		/// <param name="rnd"></param>
+		/// <param name="min"></param>
+		/// <param name="max"></param>
+		/// <returns></returns>
+        public static double Range(this System.Random rnd, double min, double max)
+        {
+            var t = System.Math.Abs(rnd.NextDouble()) / double.MaxValue;
+            var len = System.Math.Max(min, max) - System.Math.Min(min, max);
+            return min + t * len;
+        }
+
+        /// <summary>
+		/// 
+		/// </summary>
+		/// <param name="rnd"></param>
+		/// <param name="min"></param>
+		/// <param name="max"></param>
+		/// <returns></returns>
+        public static int Range(this System.Random rnd, int min, int max)
+        {
+            var len = System.Math.Max(min, max) - System.Math.Min(min, max);
+            return min + rnd.Next(len);
+        }
+
+        /// <summary>
+		/// 
+		/// </summary>
+		/// <param name="rnd"></param>
+		/// <param name="min"></param>
+		/// <param name="max"></param>
+		/// <returns></returns>
+        public static float Range(this System.Random rnd, float min, float max)
+            => (float)rnd.Range((double)min, (double)max);
+
+    }
+}
---
+++ Tests/Runtime/CSharp/Extensions/TestRandomExtensions.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Hinode.Tests.CSharp.Extensions
+{
+    /// <summary>
+	/// <seealso cref="RandomExtensions"/>
+	/// </summary>
+    public class TestRandomExtensions
+    {
+        class RangeDoubleABTestParam : IABTest
+        {
+            public double Min { get; set; }
+            public double Max { get; set; }
+
+            System.Random UseRandom { get; set; }
+            protected override (string name, string paramText)[] GetParamTexts()
+            {
+                return new (string name, string paramText)[]
+                {
+                    ("Min", Min.ToString()),
+                    ("Max", Max.ToString()),
+                };
+            }
+
+            protected override void InitParams(System.Random rnd)
+            {
+                var tmp = rnd.NextDouble();
+                var max = rnd.NextDouble();
+                Min = System.Math.Min(tmp, max);
+                Max = System.Math.Max(tmp, max);
+
+                UseRandom = rnd;
+            }
+
+            protected override void TestMethod()
+            {
+                var value = UseRandom.Range(Min, Max);
+
+                var errorMessage = $"Fail test... result={value}";
+                Assert.IsTrue(Min <= value && value <= Max, errorMessage);
+            }
+        }
+
+        /// <summary>
+		/// <seealso cref="RandomExtensions.Range(System.Random, double, double)"/>
+		/// </summary>
+        [Test]
+        public void ABTestRangeDouble()
+        {
+            var settings = TestSettings.CreateOrGet();
+            var ABTest = new RangeDoubleABTestParam();
+            ABTest.RunTest(settings);
+        }
+
+        class RangeIntABTestParam : IABTest
+        {
+            public int Min { get; set; }
+            public int Max { get; set; }
+
+            System.Random UseRandom { get; set; }
+            protected override (string name, string paramText)[] GetParamTexts()
+            {
+                return new (string name, string paramText)[]
+                {
+                    ("Min", Min.ToString()),
+                    ("Max", Max.ToString()),
+                };
+            }
+
+            protected override void InitParams(System.Random rnd)
+            {
+                var tmp = rnd.Next();
+                var max = rnd.Next();
+                Min = System.Math.Min(tmp, max);
+                Max = System.Math.Max(tmp, max);
+
+                UseRandom = rnd;
+            }
+
+            protected override void TestMethod()
+            {
+                var value = UseRandom.Range(Min, Max);
+
+                var errorMessage = $"Fail test... result={value}";
+                Assert.IsTrue(Min <= value && value <= Max, errorMessage);
+            }
+        }
+
+        /// <summary>
+        /// <seealso cref="RandomExtensions.Range(System.Random, int, int)"/>
+        /// </summary>
+        [Test]
+        public void ABTestRangeInt()
+        {
+            var settings = TestSettings.CreateOrGet();
+            var ABTest = new RangeDoubleABTestParam();
+            ABTest.RunTest(settings);
+        }
+
+        class RangeFloatABTestParam : IABTest
+        {
+            public float Min { get; set; }
+            public float Max { get; set; }
+
+            System.Random UseRandom { get; set; }
+            protected override (string name, string paramText)[] GetParamTexts()
+            {
+                return new (string name, string paramText)[]
+                {
+                    ("Min", Min.ToString()),
+                    ("Max", Max.ToString()),
+                };
+            }
+
+            protected override void InitParams(System.Random rnd)
+            {
+                var tmp = (float)rnd.NextDouble();
+                var max = (float)rnd.NextDouble();
+                Min = System.Math.Min(tmp, max);
+                Max = System.Math.Max(tmp, max);
+
+                UseRandom = rnd;
+            }
+
+            protected override void TestMethod()
+            {
+                var value = UseRandom.Range(Min, Max);
+
+                var errorMessage = $"Fail test... result={value}";
+                Assert.IsTrue(Min <= value && value <= Max, errorMessage);
+            }
+        }
+        /// <summary>
+        /// <seealso cref="RandomExtensions.Range(System.Random, float, float)"/>
+        /// </summary>
+        [Test]
+        public void ABTestRangeFloat()
+        {
+            var settings = TestSettings.CreateOrGet();
+            var ABTest = new RangeDoubleABTestParam();
+            ABTest.RunTest(settings);
+        }
+    }
+}
