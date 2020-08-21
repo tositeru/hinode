ac9014c3cf612d89afe33de993a2a274ba26b514
tositeru <tositeru104@protonmail.com>
2020-08-21 13:52:06 +0900

    IndexCombinationEnumerableを追加

---
+++ Runtime/Math/IndexCombinationEnumerable.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+using System.Linq;
+using UnityEngine.Assertions;
+
+namespace Hinode
+{
+    /// <summary>
+	/// 与えられた長さの配列に対する添字の全ての組み合わせを列挙していくEnumerable
+	///
+	/// ex) new AllEnumFlagCombinationEnumerable(3)
+	///  => 0
+	///     0, 1
+	///     0, 2
+	///     0, 1, 2
+	///     1
+	///     1, 2
+	///     2
+	/// </summary>
+    public class IndexCombinationEnumerable : IEnumerable<IEnumerable<int>>, IEnumerable
+    {
+        int _length = 0;
+        public IndexCombinationEnumerable(int length)
+        {
+            _length = length;
+        }
+
+        public IEnumerator<IEnumerable<int>> GetEnumerator()
+        {
+            int[] rowIndecies = Enumerable.Repeat(-1, _length).ToArray();
+            while (Increment(rowIndecies))
+            {
+                yield return new IndexEnumerable(rowIndecies);
+            }
+        }
+
+        bool Increment(int[] rowIndecies)
+        {
+            var index = 0;
+            for(index = rowIndecies.Length-1; index >= 0; --index)
+            {
+                if (-1 != rowIndecies[index])
+                    break;
+            }
+
+            if(index < 0)
+            {
+                rowIndecies[0] = 0;
+            }
+            else
+            {
+                if (index+1 < rowIndecies.Length && -1 == rowIndecies[index + 1]) index += 1;
+
+                if(rowIndecies[index] == -1)
+                {
+                    rowIndecies[index] = rowIndecies[index-1] + 1;
+                }
+                else
+                {
+                    rowIndecies[index]++;
+                }
+
+                if (rowIndecies[index] >= rowIndecies.Length)
+                {
+                    rowIndecies[index] = -1;
+                    for (var j = index-1; j >= 0; --j)
+                    {
+                        rowIndecies[j]++;
+                        if (rowIndecies[j] < rowIndecies.Length)
+                            break;
+                        rowIndecies[j] = -1;
+                    }
+                }
+            }
+
+            return rowIndecies[0] != -1;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+
+        class IndexEnumerable : IEnumerable<int>, IEnumerable
+        {
+            int[] _list;
+            public IndexEnumerable(int[] list)
+            {
+                _list = list;
+            }
+
+            public IEnumerator<int> GetEnumerator()
+            {
+                for(var i=0; i<_list.Length; ++i)
+                {
+                    if (_list[i] != -1) yield return _list[i];
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+        }
+
+
+        public static IEnumerable<T> GetFlagEnumCombination<T>(params T[] flags)
+            where T : System.Enum
+        {
+            return new FlagEnumCombinationEnumerable<T>(flags)
+                .Select(_f => (T)(object)_f);
+        }
+        public static IEnumerable<T> GetFlagEnumCombination<T>(IEnumerable<T> flags)
+            where T : System.Enum
+            => GetFlagEnumCombination(flags.ToArray());
+
+        class FlagEnumCombinationEnumerable<T> : IEnumerable<int>, IEnumerable
+            where T : System.Enum
+        {
+            T[] _flags;
+            public FlagEnumCombinationEnumerable(T[] flags)
+            {
+                Assert.IsNotNull(flags);
+                Assert.IsTrue(typeof(T).GetCustomAttribute<System.FlagsAttribute>() != null, $"{typeof(T)} is not System.Flag...");
+
+                _flags = flags;
+            }
+
+            public IEnumerator<int> GetEnumerator()
+            {
+                foreach(var indexPair in new IndexCombinationEnumerable(_flags.Length))
+                {
+                    yield return indexPair
+                        .Where(_i => _i != -1)
+                        .Select(_i => (int)(object)(_flags[_i]))
+                        .Aggregate((_s, _c) => _s | _c);
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+
+        }
+    }
+}
---
+++ Runtime/Math/MathUtils.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Math;
+
+namespace Hinode
+{
+    public static class MathUtils
+    {
+        /// <summary>
+		/// UnityのTest Frameworksから借用
+		/// </summary>
+		/// <param name="expected"></param>
+		/// <param name="actual"></param>
+		/// <param name="epsilon"></param>
+		/// <returns></returns>
+        public static bool AreNearlyEqual(float expected, float actual, float epsilon=float.Epsilon)
+        {
+            // special case for infinity
+            if (expected == float.PositiveInfinity || actual == float.PositiveInfinity || expected == float.NegativeInfinity || actual == float.NegativeInfinity)
+                return expected == actual;
+
+            // we cover both relative and absolute tolerance with this check
+            // which is better than just relative in case of small (in abs value) args
+            // please note that "usually" approximation is used [i.e. abs(x)+abs(y)+1]
+            // but we speak about test code so we dont care that much about performance
+            // but we do care about checks being more precise
+            return Abs(actual - expected) <= epsilon * Max(Max(Abs(actual), Abs(expected)), 1.0f);
+        }
+
+        /// <summary>
+        /// UnityのTest Frameworksから借用
+        /// </summary>
+        /// <param name="expected"></param>
+        /// <param name="actual"></param>
+        /// <param name="epsilon"></param>
+        /// <returns></returns>
+        public static bool AreNearlyEqual(double expected, double actual, double epsilon=double.Epsilon)
+        {
+            // special case for infinity
+            if (expected == double.PositiveInfinity || actual == double.PositiveInfinity || expected == double.NegativeInfinity || actual == double.NegativeInfinity)
+                return expected == actual;
+
+            // we cover both relative and absolute tolerance with this check
+            // which is better than just relative in case of small (in abs value) args
+            // please note that "usually" approximation is used [i.e. abs(x)+abs(y)+1]
+            // but we speak about test code so we dont care that much about performance
+            // but we do care about checks being more precise
+            return Abs(actual - expected) <= epsilon * Max(Max(Abs(actual), Abs(expected)), 1.0f);
+        }
+
+    }
+}
