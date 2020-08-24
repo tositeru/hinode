9bf08c5e18e38b955597afb217cd4c56aedcc459
tositeru <tositeru104@protonmail.com>
2020-08-24 19:56:30 +0900

    Layout計算の補助的なクラスであるLayoutInfoの追加

---
+++ Layouts/Runtime/LayoutInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hinode.Layouts
+{
+    /// <summary>
+    /// Layoutの基本的な情報を集めたクラス
+    /// </summary>
+    [System.Serializable]
+    public class LayoutInfo
+    {
+        public delegate void OnChangedValueDelegate(LayoutInfo self, ValueKind kind);
+
+        [System.Flags]
+        public enum ValueKind
+        {
+            UnitSize = 0x1 << 0,
+        }
+
+        SmartDelegate<OnChangedValueDelegate> _onChangedValue = new SmartDelegate<OnChangedValueDelegate>();
+
+        [SerializeField] Vector3 _unitSize;
+
+        public NotInvokableDelegate<OnChangedValueDelegate> OnChangedValue { get => _onChangedValue; }
+
+        /// <summary>
+        /// ILayoutTargetの基準となるサイズ
+        ///
+        /// ILayoutの計算内で補助的に使用されます。
+        /// </summary>
+        public Vector3 UnitSize
+        {
+            get => _unitSize;
+            set
+            {
+                if (_unitSize.AreNearlyEqual(value, LayoutDefines.NUMBER_PRECISION))
+                    return;
+                _unitSize = value;
+                _onChangedValue.SafeDynamicInvoke(this, ValueKind.UnitSize, () => $"LayoutInfo#UnitSize", LayoutDefines.LOG_SELECTOR);
+            }
+        }
+    }
+}
---
+++ Layouts/Tests/Runtime/TestLayoutInfo.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Hinode.Tests;
+
+namespace Hinode.Layouts.Tests
+{
+    /// <summary>
+    /// <seealso cref="LayoutInfo"/>
+    /// </summary>
+    public class TestLayoutInfo
+    {
+        #region UnitSize Property
+        /// <summary>
+        /// <seealso cref="LayoutInfo.UnitSize"/>
+        /// </summary>
+        [Test]
+        public void UnitSizePropertyPasses()
+        {
+            var info = new LayoutInfo();
+
+            var size = Vector3.one * 100f;
+            info.UnitSize = size;
+
+            AssertionUtils.AreNearlyEqual(size, info.UnitSize);
+        }
+
+        /// <summary>
+        /// <seealso cref="LayoutInfo.UnitSize"/>
+        /// <seealso cref="LayoutInfo.OnChangedValue"/>
+        /// </summary>
+        [Test]
+        public void OnChangedValue_UnitSizePropertyPasses()
+        {
+            var info = new LayoutInfo();
+
+            var callCounter = 0;
+            (LayoutInfo self, LayoutInfo.ValueKind kind) recievedValue = default;
+            info.OnChangedValue.Add((_self, _kind) => {
+                callCounter++;
+                recievedValue = (_self, _kind);
+            });
+
+            var size = Vector3.one * 100f;
+            info.UnitSize = size;
+
+            Assert.AreEqual(1, callCounter);
+            Assert.AreSame(info, recievedValue.self);
+            Assert.AreEqual(LayoutInfo.ValueKind.UnitSize, recievedValue.kind);
+        }
+
+        /// <summary>
+        /// <seealso cref="LayoutInfo.UnitSize"/>
+        /// <seealso cref="LayoutInfo.OnChangedValue"/>
+        /// </summary>
+        [Test]
+        public void OnChangedValue_UnitSizeProperty_WhenThrowExceptionPasses()
+        {
+            var info = new LayoutInfo();
+
+            info.OnChangedValue.Add((_self, _kind) => {
+                throw new System.Exception();
+            });
+
+            var size = Vector3.one * 100f;
+            info.UnitSize = size;
+
+            AssertionUtils.AreNearlyEqual(size, info.UnitSize);
+        }
+        #endregion
+    }
+}
