575bdca8448294debd9f3389dfd05426c3cf9b6c
tositeru <tositeru104@protonmail.com>
2020-10-09 14:36:18 +0900

    Editor機能の追加

--- Docs/Editor.md
+++ Docs/Editor.md
@@ -3,3 +3,17 @@
 Hinodeには以下のEditor拡張を提供しています。
 
 - [テキストテンプレートエンジン](./Editor/TextTemplateEngine.md)
+- [Test Class Linter](#TestClassLinter)
+
+### Test Class Linter
+
+#### TODO パーサー実装後に実装予定になります。
+
+Test Class LinterはTestClassにあるテスト関数の情報をまとめて、コメントとして出力するEditor拡張になります。
+
+Testsディレクトリ内にあるTest用のcsファイルを対象に処理を行います。
+
+使用法は以下のものになります。
+
+- Linterを実行したいテストファイルを選択
+- Assets > Hinode > Lint Test Class in selecting files をクリック
---
+++ Tests/Editor/TestClassLinter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System.IO;
+
+namespace Hinode.Tests.Editors
+{
+    /// <summary>
+    /// TODO C#パーサーの実装を行ってから実装する予定です。
+    /// </summary>
+    public class TestClassLinter
+    {
+        /// <summary>
+        /// Testsフォルダーに存在するものスクリプトのみを処理対象にします。
+        /// </summary>
+        /// <returns></returns>
+        [MenuItem("Assets/Hinode/Lint Test Class in selecting files", true)]
+        static bool ValidateTestScriptLinter()
+        {
+            return GetSelectingScriptAssetPath()
+                .Any(_path => -1 != Path.GetDirectoryName(_path).IndexOf("Tests"));
+        }
+
+        /// <summary>
+        /// TestClassのドキュメントを整理します。
+        /// </summary>
+        [MenuItem("Assets/Hinode/Lint Test Class in selecting files")]
+        public static void LintTestScript()
+        {
+            Logger.LogWarning(Logger.Priority.High, () => "This Function will implement after Implement C Sharp Parser.");
+        }
+
+        static IEnumerable<string> GetSelectingScriptAssetPath()
+        {
+            return Selection.assetGUIDs
+                .Select(_guid => AssetDatabase.GUIDToAssetPath(_guid))
+                .Where(_path => Path.GetExtension(_path) == ".cs");
+        }
+    }
+}
