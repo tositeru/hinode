d40d094f4d349eb9becafdc2b4218b8546fcbfbf
tositeru <tositeru104@protonmail.com>
2020-10-13 16:40:08 +0900

    ScenePathAttributeを追加

---
+++ Editor/Attributes/ScenePathAttributePropertyDrawer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using System.Linq;
+
+namespace Hinode.Editors
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [CustomPropertyDrawer(typeof(ScenePathAttribute))]
+    public class ScenePathAttributePropertyDrawer : PropertyDrawer
+    {
+        string[] _scenePathList = null;
+
+        public string[] ScenePathList
+        {
+            get
+            {
+                if(_scenePathList == null)
+                {
+                    _scenePathList = 
+                        EditorBuildSettings.scenes.Select(_s => _s.path)
+                        .ToArray();
+                }
+                return _scenePathList;
+            }
+        }
+
+        int FindIndex(string path)
+        {
+            var index = System.Array.FindIndex(ScenePathList, _p => _p == path);
+            return index;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var divideCount = 6;
+            var pos = new GUILayoutPosition(position);
+            var labelPos = pos.GetSplitPos(divideCount, 0, 2);
+            EditorGUI.LabelField(labelPos, label);
+
+            var popUpPos = pos.GetSplitPos(divideCount, 2, 4);
+            var selectingSceneIndex = FindIndex(property.stringValue);
+            var index = EditorGUI.Popup(popUpPos, selectingSceneIndex, ScenePathList);
+            if (selectingSceneIndex != index)
+            {
+                property.stringValue = index >= 0
+                    ? EditorBuildSettings.scenes[index].path
+                    : "";
+            }
+        }
+    }
+}
---
+++ Runtime/Attributes/ScenePathAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hinode
+{
+    /// <summary>
+    /// Sceneファイルへのパスを表す文字列に指定するAttribute
+    /// Editor上でBuildingSettingsに登録されているSceneへのPopupを表示するようにUIが切り替わります。
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    public sealed class ScenePathAttribute : PropertyAttribute
+    {
+        public ScenePathAttribute()
+        {
+        }
+    }
+}
